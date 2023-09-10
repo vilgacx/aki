ab1dbc8ceaeeb1d4e0019006c575ff6286b088cc
xorvet <cypherx702@gmail.com>
2023-09-10 22:56:49 +0530

    holy shi-

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 window.onload = () => {
-  document.querySelectorAll('[swap][url]').forEach((element) => {
-    element.onclick = () => {
-      fetch(element.getAttribute('url'))
-        .then((response) => response.text())
+  document.querySelectorAll('[target][url][fn]').forEach((element) => {
+    const attr = element.attributes;
+    element[attr.fn.value] = () => {
+      fetch(attr.url.value)
+        .then((response) => response.json())
         .then((data) => {
-          document.querySelector(element.getAttribute('swap')).innerHTML = data;
+          document.querySelector(attr.target.value)[data.change] = data.value;
         })
     }
   });
